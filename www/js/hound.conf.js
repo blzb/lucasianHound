7af45e6651088f977616e9a35dc80bb49b657a9f
Angel pimentel <angelpmza@gmail.com>
2013-02-22 10:59:20 -0600

    cambio de repositorio

--- www/js/hound.conf.js
+++
@@ -1,6 +0,0 @@
-hound.config={
-			appName:"lucasian_labs",
-			appDisplayName:"Lucasian Labs Mobile",
-			remote_server:"http://lucasian.dnsd.me:8081/HoundConnector/rs/",
-			remote_server_files:"http://lucasian.dnsd.me:8080/hound/"
-		}
