9c61a5e888e40e976eaaf9581d4b7bd831ce3e8e
Angel pimentel <angelpmza@gmail.com>
2013-03-06 12:59:52 -0600

    cambio de servidor

--- www/js/hound.conf.js
+++ www/js/hound.conf.js
@@ -1,7 +1,7 @@
 hound.config={
     appName:"lucasian_labs",
     appDisplayName:"Lucasian Labs Mobile",
-    remote_server:"http://lucasian.dnsd.me:8081/HoundConnector/rs/",
-    remote_server_files:"http://lucasian.dnsd.me:8080/hound/",
+    remote_server:"http://192.168.1.110:8281/HoundConnector/rs/",
+    remote_server_files:"http://192.168.1.110/hound/",
     senderId: "157854791681"
 }
